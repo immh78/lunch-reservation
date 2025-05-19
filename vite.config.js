6d9a678f124f0ea006d79179c35d4504ab0d95c1
immh78 <85915971+immh78@users.noreply.github.com>
2025-05-20 00:27:46 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -23,17 +23,17 @@ export default defineConfig({
         start_url: '/eng-quiz-cw/',
         icons: [
           {
-            src: 'notebook.png',
+            src: 'notebook-cw.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: 'notebook.png',
+            src: 'notebook-cw.png',
             sizes: '512x512',
             type: 'image/png'
           },
           {
-            src: 'notebook.png',
+            src: 'notebook-cw.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any maskable'
