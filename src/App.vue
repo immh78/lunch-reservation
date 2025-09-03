8d5118dde76e1c7aebcc5c806b23635e1072e462
immh78 <85915971+immh78@users.noreply.github.com>
2025-09-04 07:47:27 +0900

    font 3

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import { RouterView } from 'vue-router';
     <RouterView />
 </template>
 
-<style scoped>
+<style>
 body {
   font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
     Ubuntu, Cantarell, 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
